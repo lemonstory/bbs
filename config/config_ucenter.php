c88957597d484fcb8db50c36218161eb5f07b996
huqiangqiang <will8436992@sina.com>
2015-12-19 13:48:41 +0800

    add file

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'rdsbkno70qk52y8q9ntj.mysql.rds.aliyuncs.com');
+define('UC_DBUSER', 'share_discuz');
+define('UC_DBPW', 'bQK9RvxeTJtOhxpkLvdp');
+define('UC_DBNAME', 'share_discuz');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`share_discuz`.dx3_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'w92766LdTbs0TcVfw0m3L9M1adf4U5Mab872Deieb75cAece6fU8wcw83fF2U77a');
+define('UC_API', 'http://bbs.xiaoningmeng.net/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
