802d6941c092f7832b36a3faa6dba8d60ec39d1b
huqiangqiang <will8436992@sina.com>
2016-01-05 17:35:46 +0800

    修改用户设置

--- config/config_ucenter.php
+++ config/config_ucenter.php
@@ -3,18 +3,18 @@
 
 define('UC_CONNECT', 'mysql');
 
-define('UC_DBHOST', '');
-define('UC_DBUSER', '');
-define('UC_DBPW', '');
-define('UC_DBNAME', '');
+define('UC_DBHOST', 'rdsbkno70qk52y8q9ntj.mysql.rds.aliyuncs.com');
+define('UC_DBUSER', 'share_bbs');
+define('UC_DBPW', 'bQK9RvxeTJtOhxpkLvdp');
+define('UC_DBNAME', 'share_ucenter');
 define('UC_DBCHARSET', 'utf8');
-define('UC_DBTABLEPRE', '');
+define('UC_DBTABLEPRE', '`share_ucenter`.uc_');
 define('UC_DBCONNECT', 0);
 
 define('UC_CHARSET', 'utf-8');
-define('UC_KEY', '');
-define('UC_API', '');
+define('UC_KEY', 'K5z1p3k2U8b6B7n5i16aL6M1Yaf9ma9fT5D1A7P010Je81x61eg3v8x0W9544cS5');
+define('UC_API', 'http://uc.xiaoningmeng.net');
 define('UC_APPID', '1');
-define('UC_IP', '');
+define('UC_IP', '120.26.52.211');
 define('UC_PPP', 20);
 ?>
