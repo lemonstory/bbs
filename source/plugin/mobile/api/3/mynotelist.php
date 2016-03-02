3045f31c3e13f1d9de6fc0f677e5dcb1dde0ba69
gaoyong <gaoyong06@qq.com>
2016-03-02 16:51:32 +0800

    增加:输出帖子message

--- source/plugin/mobile/api/3/mynotelist.php
+++ source/plugin/mobile/api/3/mynotelist.php
@@ -20,12 +20,13 @@ class mobile_api {
 	}
 
 	function output() {
-		global $_G;
 
+		global $_G;
 		$noticelang = lang('notification', 'reppost_noticeauthor');
 		$noticepreg = '/^'.str_replace(array('\{actor\}', '\{subject\}', '\{tid\}', '\{pid\}'), array('(.+?)', '(.+?)', '(\d+)', '(\d+)'), preg_quote($noticelang, '/')).'$/';
 		$actorlang = '<a href="home.php?mod=space&uid={actoruid}">{actorusername}</a>';
 		$actorpreg = '/^'.str_replace(array('\{actoruid\}', '\{actorusername\}'), array('(\d+)', '(.+?)'), preg_quote($actorlang, '/')).'$/';
+
 
 		foreach($GLOBALS['list'] as $_k => $_v) {
 			if(preg_match($noticepreg, $_v['note'], $_r)) {
@@ -33,7 +34,20 @@ class mobile_api {
 				if(preg_match($actorpreg, $actor, $_r)) {
 					list(, $actoruid, $actorusername) = $_r;
 				}
+
+				//取出回帖的内容
+				include_once libfile('function/forum');
+				require_once libfile('function/discuzcode');
+				loadforum(null, $tid);
+				if($pid) {
+					$postlist = $this->post = get_post_by_pid($pid, 'message');
+					$GLOBALS['list'][$_k]['message'] = discuzcode($postlist['message']);
+				}
+
+				$GLOBALS['list'][$_k]['dateline'] = dgmdate($GLOBALS['list'][$_k]['dateline']);
+				$GLOBALS['list'][$_k]['dbdateline'] = $GLOBALS['list'][$_k]['dateline'];
 				$GLOBALS['list'][$_k]['notevar'] = array(
+					//'fid' => $fid,
 					'tid' => $tid,
 					'pid' => $pid,
 					'subject' => $subject,
@@ -43,10 +57,12 @@ class mobile_api {
 			}
 		}
 		$variable = array(
-			'list' => mobile_core::getvalues(array_values($GLOBALS['list']), array('/^\d+$/'), array('id', 'uid', 'type', 'new', 'authorid', 'author', 'note', 'dateline', 'from_id', 'from_idtype', 'from_num', 'style', 'rowid', 'notevar')),
+			'hash' => md5(substr(md5($_G['config']['security']['authkey']), 8).$_G['uid']),
+			'list' => mobile_core::getvalues(array_values($GLOBALS['list']), array('/^\d+$/'), array('id', 'uid', 'type', 'new', 'authorid', 'author', 'note', 'dateline', 'from_id', 'from_idtype', 'from_num', 'style', 'rowid', 'notevar','message')),
 			'count' => $GLOBALS['count'],
 			'perpage' => $GLOBALS['perpage'],
 			'page' => intval($GLOBALS['page']),
+
 		);
 		mobile_core::result(mobile_core::variable($variable));
 	}
